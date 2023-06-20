78d311021860ca1225b67e0bfaed2678ec95d374
s-jad <s_jad@web.de>
2023-06-20 11:35:49 +0200

    Fix: inconsistent scoring

--- scripts.js
+++ scripts.js
@@ -85,11 +85,12 @@ function controlComputerButton(computerChoice) {
     }
 }
 
-function resetButtonsScale() {
-    const buttons = document.querySelectorAll("button[style*='scale(1.1)']");
+function resetButtons() {
+    const buttons = document.querySelectorAll(".choice-button");
 
     buttons.forEach(button => {
         button.style.transform = "scale(1)";
+        button.style.border = "0px";
     });
 }
 
@@ -97,49 +98,52 @@ function getScore(playerChoice, computerChoice) {
     let playerScore = 0;
     let computerScore = 0;
 
-    if ((playerChoice + 1) % 3 === computerChoice) {
-        computerScore++;
-    } else if ((playerChoice + 2) % 3 === computerChoice) {
-        playerScore++;
+    if (playerChoice === computerChoice) {
+        return { playerScore, computerScore };
     }
 
-    return { playerScore, computerScore };
+    if ((playerChoice + 1) % 3 === computerChoice % 3) {
+        computerScore++;
+        return { playerScore, computerScore };
+    }
+
+    if ((playerChoice + 2) % 3 === computerChoice % 3) {
+        playerScore++;
+        return { playerScore, computerScore };
+    }
 }
 
 async function playOneRound() {
     const choiceDisplay = document.querySelector(".display-choice");
-    const playerButton = await getPlayerChoice();
-    playerButton.style.border = "2px solid rgba(215, 0, 30, 0.7)";
 
-    const playerChoice = parseInt(playerButton.id);
-    let computerChoice;
     // Players Turn
+    const playerButton = await getPlayerChoice();
+
+    playerButton.style.border = "2px solid rgba(215, 0, 30, 0.7)";
+    const playerChoice = parseInt(playerButton.id);
     displayChoice(playerChoice);
 
     // Computers Turn
     let computerButton;
+    let computerChoice = getComputerChoice();
 
     return new Promise(resolve => {
         setTimeout(() => {
-            resetButtonsScale();
             choiceDisplay.innerText = "Computer chooses...";
 
             setTimeout(() => {
-                computerChoice = getComputerChoice();
                 computerButton = controlComputerButton(computerChoice);
                 displayChoice(computerChoice);
 
                 setTimeout(() => {
-                    resetButtonsScale();
-                    playerButton.style.border = "0px";
-                    computerButton.style.border = "0px";
+                    resetButtons();
                     choiceDisplay.innerText = "Player chooses...";
-                    resolve({ playerChoice, computerChoice });
                 }, 1000);
 
             }, 2000);
 
         }, 2000);
+        resolve({ playerChoice, computerChoice });
     });
 }
 
@@ -151,10 +155,10 @@ async function playOneGame() {
     let playerTotal = 0;
     let computerTotal = 0;
 
-    while (playerTotal < 2 && computerTotal < 2) {
+    while (playerTotal < 3 && computerTotal < 3) {
         let choices = await playOneRound();
 
-        let {playerScore, computerScore} = getScore(choices.playerChoice, choices.computerChoice);
+        let { playerScore, computerScore } = getScore(choices.playerChoice, choices.computerChoice);
 
         playerTotal = playerTotal + playerScore;
         computerTotal = computerTotal + computerScore;
@@ -166,6 +170,14 @@ async function playOneGame() {
     setTimeout(() => {
         popup.style.display = 'block';
         popup.style.opacity = "1.0";
+
+        if (playerTotal > computerTotal) {
+            popup.innerHTML = "Congratulations, you won!";
+        } else if (playerTotal < computerTotal) {
+            popup.innerHTML = "Sorry, better luck next time!";
+        } else {
+            popup.innerHTML = "Wait, how did we end up here?!";
+        }
 
     }, 2000);
 
